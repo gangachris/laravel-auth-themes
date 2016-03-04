28a481e9911a63f7fc32e2a27130be3f589efe37
gangachris <ganga.chris@gmail.com>
2016-03-04 14:37:02 +0300

    Add Auth theme command file

---
+++ src/AuthThemesServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Ganga\AuthThemes;
+
+use Illuminate\Support\ServiceProvider;
+
+class AuthThemesServiceProvider extends ServiceProvider
+{
+
+   /**
+     * Bootstrap the application services.
+     *
+     * @return void
+    */
+    public function boot()
+    {
+        //Publishes auth theme command file to applications commands folder
+        $this->publishes([__DIR__.'/AuthThemeCommand.php' => app_path('Console/Commands/AuthThemeCommand.php')]);
+    }
+
+  /**
+    * Register the application services.
+    *
+    * @return void
+  */
+    public function register()
+    {
+       //
+    }
+}
